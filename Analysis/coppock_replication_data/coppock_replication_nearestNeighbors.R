f02f10e06ea01c3fa7e3e1e51f1bfc5401b8779a
bdesmarais <bdesmarais@psu.edu>
2018-02-22 05:22:50 -0500

    revising coppock replication

--- Analysis/coppock_replication_data/coppock_replication_nearestNeighbors.R
+++ Analysis/coppock_replication_data/coppock_replication_nearestNeighbors.R
@@ -26,14 +26,14 @@ library(permute)
 library(wnominate)
 
 permute.within.categories <- function(categories,z){
-	ucategories <- unique(categories)
-	perm.z <- rep(NA,length(z))
-	for(c in ucategories){
-		z.c <- z[which(categories==c)]
-		perm.z.c <- sample(z.c,length(z.c),rep=F)
-		perm.z[which(categories==c)] <- perm.z.c
-	}
-	perm.z
+  ucategories <- unique(categories)
+  perm.z <- rep(NA,length(z))
+  for(c in ucategories){
+    z.c <- z[which(categories==c)]
+    perm.z.c <- sample(z.c,length(z.c),rep=F)
+    perm.z[which(categories==c)] <- perm.z.c
+  }
+  perm.z
 }
 
 
@@ -104,20 +104,20 @@ expected.exp0.high <- rep(0, n)
 expected.exp1.high <- rep(0, n)
 
 for(p in 1:ncol(perm)){
-	#zp <- permute.within.categories(data$match_category,z)
+  #zp <- permute.within.categories(data$match_category,z)
   zp <- perm[,p]
-	for(i in 1:n){
-		if (zp[i] == 1){
-				expected.exp1[i] <- expected.exp1[i] + sum(S.ideo[i,]*zp)
-				expected.exp1.low[i] <- expected.exp1[i] + sum(S.ideo[i,]*zp*low_support)
-				expected.exp1.high[i] <- expected.exp1[i] + sum(S.ideo[i,]*zp*(1-low_support))
-			}
-			else{
-				expected.exp0[i] <- expected.exp0[i] + sum(S.ideo[i,]*zp)
-				expected.exp0.low[i] <- expected.exp0[i] + sum(S.ideo[i,]*zp*low_support)
-				expected.exp0.high[i] <- expected.exp0[i] + sum(S.ideo[i,]*zp*(1-low_support))
-			}
-	}
+  for(i in 1:n){
+    if (zp[i] == 1){
+      expected.exp1[i] <- expected.exp1[i] + sum(S.ideo[i,]*zp)
+      expected.exp1.low[i] <- expected.exp1[i] + sum(S.ideo[i,]*zp*low_support)
+      expected.exp1.high[i] <- expected.exp1[i] + sum(S.ideo[i,]*zp*(1-low_support))
+    }
+    else{
+      expected.exp0[i] <- expected.exp0[i] + sum(S.ideo[i,]*zp)
+      expected.exp0.low[i] <- expected.exp0[i] + sum(S.ideo[i,]*zp*low_support)
+      expected.exp0.high[i] <- expected.exp0[i] + sum(S.ideo[i,]*zp*(1-low_support))
+    }
+  }
 }
 
 num_treat <- apply(perm,1,sum)
@@ -130,17 +130,17 @@ expected.exp0 <- expected.exp0/num_control
 #### This is the spillover effect model
 
 indirect.treatment <- function(permutation, adj.mat,expected.exp0.low,expected.exp1.low,expected.exp0.high,expected.exp1.high,low_support){ #any treatment assignment vector and adjacency matrix can be used
- # permutation: can be the initial treatment assignment or a permutation
- #for (i in 1:n){
+  # permutation: can be the initial treatment assignment or a permutation
+  #for (i in 1:n){
   # raw.exp[i] <- sum(adj.mat[i,]*permutation)
   # }
- raw.exp.low <- c(adj.mat%*%(permutation*low_support))
- raw.exp.high <- c(adj.mat%*%(permutation*(1-low_support)))
- net.exp.low <- raw.exp.low - (permutation*expected.exp1.low + (1-permutation)*expected.exp0.low)
- net.exp.high <- raw.exp.high - (permutation*expected.exp1.high + (1-permutation)*expected.exp0.high)
- standard.exp.low <- (net.exp.low - mean(net.exp.low))/sd(net.exp.low) #this is the spillover or indirect effect
- standard.exp.high <- (net.exp.high - mean(net.exp.high))/sd(net.exp.high) #this is the spillover or indirect effect
- return(list(standard.exp.low,standard.exp.high,raw.exp.low,raw.exp.high))
+  raw.exp.low <- c(adj.mat%*%(permutation*low_support))
+  raw.exp.high <- c(adj.mat%*%(permutation*(1-low_support)))
+  net.exp.low <- raw.exp.low - (permutation*expected.exp1.low + (1-permutation)*expected.exp0.low)
+  net.exp.high <- raw.exp.high - (permutation*expected.exp1.high + (1-permutation)*expected.exp0.high)
+  standard.exp.low <- (net.exp.low - mean(net.exp.low))/sd(net.exp.low) #this is the spillover or indirect effect
+  standard.exp.high <- (net.exp.high - mean(net.exp.high))/sd(net.exp.high) #this is the spillover or indirect effect
+  return(list(standard.exp.low,standard.exp.high,raw.exp.low,raw.exp.high))
 }
 
 
@@ -189,33 +189,33 @@ test.stat <- sum((lm(y.z ~ eval(z*low_support)+eval(z*(1-low_support)) + exposur
 pval <- numeric(nrow(parameters))
 
 for(i in 1:nrow(parameters)){
-   
-    perm.y.0 <- z.to.unif(outcome=y.z, beta1=parameters[i,1], beta2=parameters[i,2], beta3=parameters[i,3], beta4=parameters[i,4], permutation=z, adj.mat=S.ideo,expected.exp0.low,expected.exp1.low,expected.exp0.high,expected.exp1.high,low_support)
-    #perm.y.0 <- z.to.unif(outcome = y.z, beta1 = beta1s[i], beta2 = beta2s[j], permutation = z, adj.mat = S.ideo)
-    
-    # Calculate a vector of test statistic using permutations
-    
-    beta1 <- parameters[i,1]
-    beta2 <- parameters[i,2]
-    beta3 <- parameters[i,3]
-    beta4 <- parameters[i,4]
-    
-    perm.test.stats <- numeric(perms.test)
-    
-    for(k in 1:perms.test){
-      #perm.z <- Z_block[,sample(1:10000, 1)]
-      perm.z <- perm[,sample(1:perms, 1)]
-      perm.exposure <- indirect.treatment(permutation = perm.z, adj.mat = S.ideo,expected.exp0.low,expected.exp1.low,expected.exp0.high,expected.exp1.high,low_support)
-      exposure_low <- perm.exposure[[3]]
-      exposure_high <- perm.exposure[[4]]
-      y.sim <- perm.y.0+beta1*perm.z*low_support + beta2*perm.z*(1-low_support) + beta3*exposure_low + beta4*exposure_high
-      perm.test.stats[k] <- sum((lm(y.sim ~ eval(perm.z*low_support)+eval(perm.z*(1-low_support))+exposure_low+exposure_high , na.action = na.omit)$resid)^2)
-      }
-
-    # Calculating p-value
-    pval[i] <- sum(perm.test.stats < test.stat)/perms.test
-    
-    if(i/100==round(i/100)) save(list=c("pval","parameters"),file="CoppockSPPQRRresults.RData")
+  
+  perm.y.0 <- z.to.unif(outcome=y.z, beta1=parameters[i,1], beta2=parameters[i,2], beta3=parameters[i,3], beta4=parameters[i,4], permutation=z, adj.mat=S.ideo,expected.exp0.low,expected.exp1.low,expected.exp0.high,expected.exp1.high,low_support)
+  #perm.y.0 <- z.to.unif(outcome = y.z, beta1 = beta1s[i], beta2 = beta2s[j], permutation = z, adj.mat = S.ideo)
+  
+  # Calculate a vector of test statistic using permutations
+  
+  beta1 <- parameters[i,1]
+  beta2 <- parameters[i,2]
+  beta3 <- parameters[i,3]
+  beta4 <- parameters[i,4]
+  
+  perm.test.stats <- numeric(perms.test)
+  
+  for(k in 1:perms.test){
+    #perm.z <- Z_block[,sample(1:10000, 1)]
+    perm.z <- perm[,sample(1:perms, 1)]
+    perm.exposure <- indirect.treatment(permutation = perm.z, adj.mat = S.ideo,expected.exp0.low,expected.exp1.low,expected.exp0.high,expected.exp1.high,low_support)
+    exposure_low <- perm.exposure[[3]]
+    exposure_high <- perm.exposure[[4]]
+    y.sim <- perm.y.0+beta1*perm.z*low_support + beta2*perm.z*(1-low_support) + beta3*exposure_low + beta4*exposure_high
+    perm.test.stats[k] <- sum((lm(y.sim ~ eval(perm.z*low_support)+eval(perm.z*(1-low_support))+exposure_low+exposure_high , na.action = na.omit)$resid)^2)
+  }
+  
+  # Calculating p-value
+  pval[i] <- sum(perm.test.stats < test.stat)/perms.test
+  
+  if(i/100==round(i/100)) save(list=c("pval","parameters"),file="CoppockSPPQRRresults.RData")
 }
 
 estimate <- c(parameters[which.max(pval),])
